92cd96ef0db42cdea1300af6cadb8949c99ca787
phagoth <olexandr.averyanov@gmail.com>
2015-09-17 15:51:16 +0300

    Validation added

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,28 @@ class Task < ActiveRecord::Base
 
   scope :incompleted, -> { where('is_completed=false') }
   scope :completed,   -> { where('is_completed=true') }
-end
 
+  validates :title, presence: true
+  validates :priority, presence: true, numericality: {only_integer: true, greater_than: 0}
+  validates :due_date, presence: true, if: :is_future_date?
+
+  before_validation :is_nil?
+  
+  before_save do
+    self.is_completed = false;
+    true
+  end
+
+protected
+  def is_nil?
+    if due_date.nil?
+      errors.add(:due_date, "not a valid date!")
+      false
+    end
+  end
+
+  def is_future_date?
+    errors.add(:due_date, "must be future date!") unless due_date > Time.now
+  end
+
+end
